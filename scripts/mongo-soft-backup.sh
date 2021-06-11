12f3162d07fa69567d8a96dfbb72424c7340144c
Marc Peral <m.peral@sapalomera.cat>
2021-06-11 21:47:34 +0200

    afegida compressio al fitxer que es genera

--- scripts/mongo-soft-backup.sh
+++ scripts/mongo-soft-backup.sh
@@ -3,8 +3,8 @@
 data=$(date '+%d-%m-%Y')
 fitxer="mongo-$data"
 # Creacio del backup
-ssh -i /etc/ssh/ssh_host_rsa_key 10.132.0.10 "mongodump -u root -p prFeUqWL6hchFm8q --authenticationDatabase=admin -d nodebb && tar -cf $fitxer.tar dump && rm -r dump"
+ssh -i /etc/ssh/ssh_host_rsa_key 10.132.0.10 "mongodump -u root -p prFeUqWL6hchFm8q --authenticationDatabase=admin -d nodebb && tar -czf $fitxer.tar.gz dump && rm -r dump"
 # Copia del backup
-scp -i /etc/ssh/ssh_host_rsa_key 10.132.0.10:/root/"$fitxer.tar" "/backups/mongodb/$fitxer.tar"
+scp -i /etc/ssh/ssh_host_rsa_key 10.132.0.10:/root/"$fitxer.tar.gz" "/backups/mongodb/$fitxer.tar.gz"
 # Eliminacio del fitxer temporal en el servidor de bases de dades
-ssh -i /etc/ssh/ssh_host_rsa_key 10.132.0.10 "rm $fitxer.tar"
+ssh -i /etc/ssh/ssh_host_rsa_key 10.132.0.10 "rm $fitxer.tar.gz"
